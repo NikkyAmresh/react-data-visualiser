b9af885d85ebeffaff240d6c5bd5f4d8bcef3859
Nikky Amresh <nikkyamresh8@gmail.com>
2022-12-06 18:15:35 +0530

    add

---
+++ build/precache-manifest.47afd4e4ccf02e9cecdaa35975485054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbb6d6544dcdbd9d6abf8b7bb3e3a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d90b29111dccff5d168",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bdce4f0d717e772faf5c",
+    "url": "/static/js/2.183ec54f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.183ec54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d90b29111dccff5d168",
+    "url": "/static/js/main.0423e90c.chunk.js"
+  },
+  {
+    "revision": "d309a0ba7d589734a4fc",
+    "url": "/static/js/runtime-main.4b934ab4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Line from './Line';
-import Pie from './Pie';
-import Polar from './Polar';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Line from "./Line";
+import Pie from "./Pie";
+import Polar from "./Polar";
 
 function App() {
   return (
     <div className="App">
-      
       <Polar />
+      <Pie />
+      <Line />
     </div>
   );
 }
